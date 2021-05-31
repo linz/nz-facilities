95e5697cb7c61374a8463c6b6e9d8e4903a993e5
Pete <pking@linz.govt.nz>
2021-06-01 09:09:06 +1200

    feat: query to create and populate table for the lds

---
+++ db/sql/facilities_lds/schema_and_tables.sql
@@ -0,0 +1,31 @@
+--Schema
+
+CREATE SCHEMA IF NOT EXISTS facilities_lds;
+
+COMMENT ON SCHEMA facilities_lds IS
+'Schema that holds facilities data to publish on the LDS.';
+-- Tables
+
+-- nz_facilities
+
+-- The nz_facilities table holds a copy of the facilities.facilities table
+-- minus the internal and internal_comments fields.
+
+DROP TABLE IF EXISTS facilities_lds.nz_facilities;
+
+CREATE TABLE facilities_lds.nz_facilities as
+SELECT
+      facility_id
+    , external_facility_id
+    , name
+    , external_name
+    , use
+    , use_type
+    , use_subtype
+    , estimated_occupancy
+    , last_modified
+    , shape
+FROM facilities.facilities;
+
+CREATE INDEX shx_nz_facilities
+ON facilities_lds.nz_facilities USING gist (shape);
