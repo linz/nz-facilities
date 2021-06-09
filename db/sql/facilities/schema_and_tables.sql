289e5b4f2378b82b7b9864922b0eb0418a8da2bd
Jan Ducnuigeen <jducnuigeen@linz.govt.nz>
2021-06-10 09:28:40 +1200

    fix: updated tables

--- db/sql/facilities/schema_and_tables.sql
+++ db/sql/facilities/schema_and_tables.sql
@@ -32,19 +32,19 @@ COMMENT ON TABLE facilities.facilities IS
 COMMENT ON COLUMN facilities.facilities.facility_id IS
 'The unique identifier for each geometry.';
 COMMENT ON COLUMN facilities.facilities.source_facility_id IS
-'The unique identifier of this facility used by the authoritative source';
+'The facility unique identifier used by the authoritative source';
 COMMENT ON COLUMN facilities.facilities.name IS
-'The name of the facility.';
+'The facility name which has consistent naming convention applied. This name may directly match the name provided by the authoritative source.';
 COMMENT ON COLUMN facilities.facilities.source_name IS
 'The name of the facility used by the authoritative source.';
 COMMENT ON COLUMN facilities.facilities.use IS
 'The generic use of the facility.';
 COMMENT ON COLUMN facilities.facilities.use_type IS
-'Use type as defined by the authoritative source.';
+'Type of use as defined by the authoritative source.';
 COMMENT ON COLUMN facilities.facilities.use_subtype IS
-'Use subtype as defined by the authoritative source.';
+'Sub-type of use as defined by the authoritative source.';
 COMMENT ON COLUMN facilities.facilities.estimated_occupancy IS
-'An approximation of the occupancy from the authoritative source. It may not include staff.';
+'An approximation of the occupancy of the facility from the authoritative source, where it is known. It may not include staff.';
 COMMENT ON COLUMN facilities.facilities.last_modified IS
 'The most recent date on which any attribute or geometry that is part of the facility was modified.';
 COMMENT ON COLUMN facilities.facilities.internal IS
--- db/sql/facilities_lds/schema_and_tables.sql
+++ db/sql/facilities_lds/schema_and_tables.sql
@@ -36,17 +36,17 @@ COMMENT ON COLUMN facilities_lds.nz_facilities.facility_id IS
 COMMENT ON COLUMN facilities_lds.nz_facilities.source_facility_id IS
 'The unique identifier of this facility used by the authoritative source';
 COMMENT ON COLUMN facilities_lds.nz_facilities.name IS
-'The name of the facility.';
+'The facility name which has consistent naming convention applied. This name may directly match the name provided by the authoritative source.';
 COMMENT ON COLUMN facilities_lds.nz_facilities.source_name IS
 'The name of the facility used by the authoritative source.';
 COMMENT ON COLUMN facilities_lds.nz_facilities.use IS
 'The generic use of the facility.';
 COMMENT ON COLUMN facilities_lds.nz_facilities.use_type IS
-'Use type as defined by the authoritative source.';
+'Type of use as defined by the authoritative source.';
 COMMENT ON COLUMN facilities_lds.nz_facilities.use_subtype IS
-'Use subtype as defined by the authoritative source.';
+'Sub-type of use as defined by the authoritative source.';
 COMMENT ON COLUMN facilities_lds.nz_facilities.estimated_occupancy IS
-'An approximation of the occupancy from the authoritative source. It may not include staff.';
+'An approximation of the occupancy of the facility from the authoritative source, where it is known. It may not include staff.';
 COMMENT ON COLUMN facilities_lds.nz_facilities.last_modified IS
 'The most recent date on which any attribute or geometry that is part of the facility was modified.';
 COMMENT ON COLUMN facilities_lds.nz_facilities.shape IS
